270b45dd928405cfc28650fa70881f0586611bfd
Elkin <elquin50000@gmail.com>
2023-03-27 00:38:35 -0500

    Typescript Error Fix

--- backend/index.ts
+++ backend/index.ts
@@ -20,7 +20,7 @@ app.use(express.json())
 const whiteList = [process.env.FRONTEND_URL]
 
 const corsOptions = {
-    origin: function(origin, callback) {
+    origin: function(origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
         if (!origin) return callback(null, true);
 
         if(whiteList.includes(origin)) {
